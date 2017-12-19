a468557db99dd8b04bec81db255743ced3fe3722
Jason Hughes <jasonhughes088@gmail.com>
2017-12-19 11:33:12 -0700

    get react redux and router setup

---
+++ src/Containers/Search/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const Search = () => {
+  return(
+    <div>
+      <h1>Search</h1>
+      <input placeholder='Search for your dog here!' />
+    </div>
+  )
+}
+
+
+export default Search;
