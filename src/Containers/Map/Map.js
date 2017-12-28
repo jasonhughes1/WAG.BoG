7102d489d82927c327a97323e7690a0466132753
Jason Hughes <jasonhughes088@gmail.com>
2017-12-28 13:12:27 -0700

    change location to match what is in store

--- src/Containers/Map/Map.js
+++ src/Containers/Map/Map.js
@@ -2,29 +2,47 @@
 import css from './Map.css';
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
+import { connect } from 'react-redux';
 
 
 const showMap = ({ text }) => <div>{ text }</div>;
 
 
-export default class Map extends Component {
+class Map extends Component {
   static defaultProps = {
     center: { lat: 40.7446790, lng: -73.9485420 },
     zoom: 10
   }
-render() {
+
+
+  render() {
+    if(!this.props.location.length) {
+      return(
+        <div>wait</div>
+      )
+    } else {
+      console.log(this.props.location[0].lng);
     return (
       <div className='google-map'>
         <GoogleMapReact
           defaultCenter={ this.props.center }
           defaultZoom={ this.props.zoom }>
           <showMap
-            lat={ 40.7473310 }
-            lng={ -73.8517440 }
+            lat={ this.props.location[0].lat }
+            lng={ this.props.location[0].lng }
             text={ 'Where Waldo?' }
           />
         </GoogleMapReact>
       </div>
-    )
+      )
+    }
   }
 }
+
+const mapStateToProps = (store) => {
+  return {
+    location: store.location
+  }
+}
+
+export default connect(mapStateToProps, null)(Map);
